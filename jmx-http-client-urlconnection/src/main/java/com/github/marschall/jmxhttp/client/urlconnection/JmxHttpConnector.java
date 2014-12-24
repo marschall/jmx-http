9f4366df01668efd78a739d669a571261a2d8f28
Philippe Marschall <philippe.marschall@gmail.com>
2014-12-24 11:43:48 +0100

    minimal state machine

--- jmx-http-client-urlconnection/src/main/java/com/github/marschall/jmxhttp/client/urlconnection/JmxHttpConnector.java
+++ jmx-http-client-urlconnection/src/main/java/com/github/marschall/jmxhttp/client/urlconnection/JmxHttpConnector.java
@@ -16,6 +16,8 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import javax.management.ListenerNotFoundException;
 import javax.management.MBeanServerConnection;
@@ -27,11 +29,21 @@ import javax.security.auth.Subject;
 
 final class JmxHttpConnector implements JMXConnector {
   
+  enum State {
+    INITIAL,
+    CONNECTED,
+    CLOSED;
+  }
+  
   private static final AtomicInteger ID_GENERATOR = new AtomicInteger(1);
 
   private final URL url;
   
   private final int id;
+  
+  private final Lock sateLock;
+  
+  private State state;
 
   // TODO Auto-generated method stub
   private final List<Subscription> connectionNotificationListeners;
@@ -42,6 +54,9 @@ final class JmxHttpConnector implements JMXConnector {
     this.url = url;
     this.connectionNotificationListeners = new CopyOnWriteArrayList<>();
     this.id = ID_GENERATOR.incrementAndGet();
+    this.sateLock = new ReentrantLock();
+    this.state = State.INITIAL;
+    
   }
 
   @Override
@@ -51,11 +66,23 @@ final class JmxHttpConnector implements JMXConnector {
   }
 
   @Override
-  public synchronized void connect(Map<String, ?> env) throws IOException {
-    // TODO check if already called
-    Optional<String> credentials = extractCredentials(env);
-    this.mBeanServerConnection = new JmxHttpConnection(
-        (HttpURLConnection) this.url.openConnection(), credentials);
+  public void connect(Map<String, ?> env) throws IOException {
+    this.sateLock.lock();
+    try {
+      if (this.state == State.CONNECTED) {
+        return;
+      }
+      if (this.state == State.CLOSED) {
+        throw new IOException("already closed");
+      }
+      
+      this.state = State.CONNECTED;
+      Optional<String> credentials = extractCredentials(env);
+      this.mBeanServerConnection = new JmxHttpConnection(
+          (HttpURLConnection) this.url.openConnection(), credentials);
+    } finally {
+      this.sateLock.unlock();
+    }
 
   }
 
@@ -87,9 +114,18 @@ final class JmxHttpConnector implements JMXConnector {
   }
 
   @Override
-  public synchronized void close() throws IOException {
-    if (this.mBeanServerConnection != null) {
-      this.mBeanServerConnection.close();
+  public void close() throws IOException {
+    this.sateLock.lock();
+    try {
+      if (this.state == State.CLOSED) {
+        return;
+      }
+      this.state = State.CLOSED;
+      if (this.mBeanServerConnection != null) {
+        this.mBeanServerConnection.close();
+      }
+    } finally {
+      this.sateLock.unlock();
     }
 
   }

acb860b621c4464818c9f3a8dbac53eef6cfe767
Philippe Marschall <philippe.marschall@gmail.com>
2014-12-25 14:40:22 +0100

    - fix serialization - shade

--- jmx-http-common/src/main/java/com/github/marschall/jmxhttp/common/command/Command.java
+++ jmx-http-common/src/main/java/com/github/marschall/jmxhttp/common/command/Command.java
@@ -1,11 +1,12 @@
 package com.github.marschall.jmxhttp.common.command;
 
 import java.io.IOException;
+import java.io.Serializable;
 
 import javax.management.JMException;
 import javax.management.MBeanServerConnection;
 
-public interface Command<R> {
+public interface Command<R> extends Serializable {
 
   R execute(MBeanServerConnection connection) throws JMException, IOException;
   

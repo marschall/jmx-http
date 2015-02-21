e1f6104dd8df0099c44839f1d008b468ef676fff
Philippe Marschall <philippe.marschall@gmail.com>
2015-02-21 14:05:21 +0100

    add debug line

--- jmx-http-client-urlconnection/start-visualvm.sh
+++ jmx-http-client-urlconnection/start-visualvm.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 jvisualvm --cp:p "$PWD/$(dirname "$0")"/target/jmx-http-client-urlconnection-1.0.0-SNAPSHOT.jar
+# jvisualvm --cp:p "$PWD/$(dirname "$0")"/target/jmx-http-client-urlconnection-1.0.0-SNAPSHOT.jar -J-Xdebug -J-Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=y

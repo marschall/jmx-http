22bfc641a943237521c5bd58147b8a3b8452b27a
Philippe Marschall <philippe.marschall@gmail.com>
2015-05-08 17:37:20 +0200

    use version from maven

--- jmx-http-client-urlconnection/start-visualvm.sh
+++ jmx-http-client-urlconnection/start-visualvm.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-jvisualvm --cp:p "$PWD/$(dirname "$0")"/target/jmx-http-client-urlconnection-1.0.0-SNAPSHOT.jar
+
+PROJECT_VERSION=$(mvn help:evaluate -o -Dexpression=project.version | egrep -v '^\[|Downloading:')
+jvisualvm --cp:p "$PWD/$(dirname "$0")"/target/jmx-http-client-urlconnection-${PROJECT_VERSION}.jar
+# jvisualvm --cp:p "$PWD/$(dirname "$0")"/target/jmx-http-client-urlconnection-1.0.0-SNAPSHOT.jar
 # jvisualvm --cp:p "$PWD/$(dirname "$0")"/target/jmx-http-client-urlconnection-1.0.0-SNAPSHOT.jar -J-Xdebug -J-Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=y
